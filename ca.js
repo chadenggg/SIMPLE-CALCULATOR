b1ba7d93b60c8e39ada6fdb92abcdd130ec6cad5
Cha De Mesa <demesacha@yahoo.com.ph>
2016-07-20 07:09:39 -0700

    Add files via upload

---
+++ ca.js
@@ -0,0 +1,12 @@
+function num1(){
+
+	document.getElementById('numm1').innerHTML="1" ;
+}
+function num2(){
+	document.getElementById('numm2').innerHTML="2" ;
+}
+
+function output(x){
+	var screen = getElementById('asdf');
+	var screen.value = ';'
+}
